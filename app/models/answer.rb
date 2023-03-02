b73539c885735fba6abe6c3e612d28c6ccd81c9f
vitalikney <italikney@yandex.ru>
2023-03-02 22:00:22 +0300

    Lr11

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -25,21 +25,16 @@ class Answer < ApplicationRecord
     ActiveSupport::JSON.encode(process)
   end
 
-  # метод возвращает шестизначное число, добавляя недостающие цифры
-  def corrector(param_num)
-    param_num.insert(0, 0) until param_num.length >= 6
-    param_num # возвращаем число в виде массива
+  def palindrome?(n)
+    return true if n == n.reverse
+    return false
   end
 
   def process
-    @result = Enumerator.new do |element| # list - объект класса Enumerator
-      iter = 0
+    @result = Enumerator.new do |element| # @result - объект класса Enumerator
       n = @input.to_i
-      (0..n).cycle(1) do |num| # метод примеси Enumerable
-        iter += 1
-        # передаём в метод число в виде массива цифр и получаем массив цифр
-        number = corrector(num.digits.reverse)
-        element << [iter, num] if number.take(3).sum == number.reverse.take(3).sum
+      (0..n).cycle(1) do |x| # метод примеси Enumerable
+        element << [x, x*x] if palindrome?(x.to_s) && palindrome?((x*x).to_s)
       end
     end
   end
